bd57944ea9a7c399dc2019464d41c99d3de2cc1f
Bala Clark <nandasunu@gmail.com>
2010-07-23 11:47:04 +0100

    updated example rule

--- examples/config/rules.php
+++ examples/config/rules.php
@@ -1,37 +1,5 @@
 <?php
 
-/**
- * Define validation rules for each form here. Use the form name attribute as
- * the array key, seperate multiple validation rules with pipes: "required|username"
- *
- * Available validation methods:
- *
- *   - required
- *   - requiredif[field=value]
- *   - email
- *   - username
- *   - confirm[element(str)]
- *   - numeric
- *   - length[length(int)]
- *   - minlength[length(int)]
- *   - maxlength[length(int)]
- *
- * requiredif useage
- * The required method can also be set to conditional based on the value of another field.
- * The following operators can be used: + , = , != , < , > , <= , >= (currently only "=" is implemented, more to come...)
- *   e.g. $validation['new_name'] = 'requiredif[name=new]';
- *
- * Multiple conditions can be chained with "&":
- *   e.g. $validation['new_name'] = 'requiredif[name=new&user=yes]';
- *
- * Array Format:
- *
- * 	   $validation['form_name'] = array(
- * 		  'username' => 'required',
- * 		  'password' => 'required|password'
- * 	   );
- */
-
 $validation['basic'] = array(
 	'name' => 'required',
 	'email' => 'required|email'
@@ -41,5 +9,5 @@ $validation['advanced'] = array(
 	'name' => 'required',
 	'password' => 'required|minlength[3]|maxlength[6]',
 	'password2' => 'confirm[password]',
-	'email' => 'requiredif[optin=yes]'
+	'email' => 'requiredif[optin=yes]|email'
 );
